9f42e841d8f152ee11a91b4e00cd957c342b0058
Mohammed Mebarek Mecheter <mohamedmebarek2@gmail.com>
2023-01-29 19:56:26 +0100

    Initial commit

---
+++ py_crash/ch3/motorcycles.py
@@ -0,0 +1,17 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
